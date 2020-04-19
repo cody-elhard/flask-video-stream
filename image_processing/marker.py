309b86309b3f26d3153c08c2ab4582d7c5424f41
Tucker Bergquist <61253021+Tucker-WB@users.noreply.github.com>
2020-04-19 14:15:10 -0500

    Marker.py now returns an area of doubles

--- image_processing/marker.py
+++ image_processing/marker.py
@@ -9,137 +9,159 @@ def angle(line):
     radians = np.arctan2(x_diff, y_diff)
     return np.abs(np.degrees(radians))
 
+
+def find_contours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    bounding_box = []
+
+    for contour in contours:
+        if len(contour) > min_contour_size:
+            c = contour.squeeze(1)
+
+            min_x = np.min(c[:, 0])
+            max_x = np.max(c[:, 0])
+            min_y = np.min(c[:, 1])
+            max_y = np.max(c[:, 1])
+
+            # bounding box of contour
+            bounding_box.append((min_x, min_y, max_x, max_y))
+
+    return bounding_box
+
+
+def find_area_of_interest(markers):
+    # markers = [[marker], [marker], [marker], ....]
+    # marker = [topX, topY, btmX, btmY]
+    img_points = []
+
+    for i in range(0, len(markers), 2):
+
+        top_marker = -1
+        btm_marker = -1
+
+        # if avg Y of markers[i] is greater than avg Y of markers[i+1] (i is lower in image)
+        # then top_marker gets markers[ i + 1 ]
+        if (markers[i][1] + markers[i][3]) / 2 > (markers[i + 1][1] + markers[i + 1][3]) / 2:
+            top_marker = markers[i + 1]
+            btm_marker = markers[i]
+        else:
+            top_marker = markers[i]
+            btm_marker = markers[i + 1]
+
+        # determine range of x
+        left_x = min(top_marker[0], btm_marker[0])
+        right_x = max(top_marker[2], btm_marker[2])
+
+        # determine range of y
+        top_y = max(top_marker[1], top_marker[3])
+        btm_y = min(btm_marker[1], btm_marker[3])
+
+        # the plus/minus 1 can be removed, but canny is
+        # picking up the edges of the markers
+        img_points.append((left_x, top_y + 1, right_x, btm_y - 1))
+
+    print(img_points)
+    return img_points
+
+
+def find_water_lvls(img, areas_of_interest):
+
+    #img_points is a 2d array of 'AREAS' of Interest
+    percents = []
+
+    #for each area of interest
+    for points in areas_of_interest:
+        percent = find_water_lvl(img, points)
+        percents.append(percent)
+
+    return percents
+
+
+def find_water_lvl(img, points):
+
+    # points represents area of interest
+    # i.e. under top marker and above bottom marker
+    pts_one = np.float32([[points[0], points[1]],  # leftX  topY
+                          [points[2], points[1]],  # rightX topY
+                          [points[0], points[3]],  # leftX  btmY
+                          [points[2], points[3]]])  # rightX btmY
+
+    x_diff = points[2] - points[0]
+    y_diff = points[3] - points[1]
+
+    pts_two = np.float32([[0, 0],
+                          [x_diff, 0],
+                          [0, y_diff],
+                          [x_diff, y_diff]])
+
+    matrix = cv2.getPerspectiveTransform(pts_one, pts_two)
+    tube = cv2.warpPerspective(np.copy(img), matrix, (x_diff, y_diff))
+
+    # get edges on the tube (cropped image)
+    # might need gray scale/median filter?
+    edges = cv2.Canny(tube, 50, 150)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 15, minLineLength=(0.1 * x_diff))
+
+    for line in lines:
+        #only find one line that fits criteria
+        if 85 <= angle(line) <= 100:
+
+            x1, y1, x2, y2 = line[0]
+
+            img_topY = points[1]
+            img_btmY = points[3]
+
+            lvl = round(((y1 + y2) / 2) / (img_btmY - img_topY), 4)
+            percent = round(1 - lvl, 4)
+            #print(percent*100)
+            return percent
+
+
+
 def process_image(img, hardcoded_image = False):
-  if (hardcoded_image):
-    img = cv2.imread('images/markers.jpg')
+    if (hardcoded_image):
+        img = cv2.imread('images/markers.jpg')
 
-  #resize variables
-  width = int(img.shape[1] * .5)
-  height = int(img.shape[0] * .5)
-  dim = (width, height)
+    # resize variables
+    width = int(img.shape[1] * .5)
+    height = int(img.shape[0] * .5)
+    dim = (width, height)
 
-  # allowed variance on green/yellow #
-  yellow = [[0, 153, 153], [102, 255, 255]]
-  green = [[4,69,0], [118,185,4]]
-  lower = np.array(yellow[0])
-  upper = np.array(yellow[1])
+    # allowed variance on yellow 
+    yellow = [[0, 153, 153], [102, 255, 255]]
+    lower = np.array(yellow[0])
+    upper = np.array(yellow[1])
 
-  # bitwise and with green/yellow
-  mask = cv2.inRange(img, lower, upper)
-  output = cv2.bitwise_and(img, img, mask=mask)
+    # bitwise and with green/yellow
+    mask = cv2.inRange(img, lower, upper)
+    output = cv2.bitwise_and(img, img, mask=mask)
 
-  #black image
-  blank_image = np.zeros(shape=[img.shape[0], img.shape[1], 3], dtype=np. uint8)
+    # convert to grayscale
+    gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
 
-  ########################################################
+    # find contours on markers
+    bounding_boxes = find_contours(thresh)
 
-  # convert to grayscale
-  gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-  ret, thresh = cv2.threshold(gray, 127, 255, 0)
+    # structing the array allows for easy sorting
+    dtype = [('TopX', int), ('TopY', int), ('BottomX', int), ('BottomY', int)]
+    markers = np.array(bounding_boxes, dtype=dtype)
+    markers = np.sort(markers, order='TopX')
 
-  # find contours on markers
-  contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-  bounding_box = []
-  for contour in contours:
-      colorR = rand.randint(0 , 255)
-      colorG = rand.randint(0, 255)
-      colorB = rand.randint(0, 255)
-      if len(contour) > 200:
-          c = contour.squeeze(1)
-          min_x = np.min(c[:, 0])
-          max_x = np.max(c[:, 0])
-          min_y = np.min(c[:, 1])
-          max_y = np.max(c[:, 1])
+    areas_of_interest = find_area_of_interest(markers)
 
-          #bounding box of contour
-          start = (min_x, min_y)
-          end = (max_x, max_y)
-          bounding_box.append((min_x, min_y, max_x, max_y))
+    # same concept for the structure as bounding boxes
+    img_points = np.array(areas_of_interest, dtype=dtype)
 
-          #show bounding box
-          cv2.rectangle(img, start, end, (0, 255, 0), 4)
+    # tubes = [len(img_points)]
 
-          #show contours
-          cv2.drawContours(blank_image, contour, -1, (colorR, colorG, colorB), 3)
+    print('\nNumber of tubes detected: ' + str(len(img_points)))
 
-  #structing the array allows for easy sorting
-  dtype = [('TopX', int), ('TopY', int), ('BottomX', int), ('BottomY', int)]
-  bounding_box = np.array(bounding_box, dtype=dtype)
-  bounding_box = np.sort(bounding_box, order='TopX')
-  print(bounding_box)
+    water_lvl_percents = find_water_lvls(img, img_points)
 
-  img_points = []
+    print(water_lvl_percents)
 
-  for i in range(0, len(bounding_box), 2):
-      top_marker = -1
-      btm_marker = -1
-
-      #if avg Y of i is greater than avg Y of i+1 (i is lower in image)
-      #then top_marker gets i + 1
-      if (bounding_box[i][1] + bounding_box[i][3])/2 > (bounding_box[i+1][1] + bounding_box[i+1][3])/2:
-          top_marker = bounding_box[i + 1]
-          btm_marker = bounding_box[i]
-      else:
-          top_marker = bounding_box[i]
-          btm_marker = bounding_box[i + 1]
-
-      #determine range of x
-      left_x = min(top_marker[0], btm_marker[0])
-      right_x = max(top_marker[2], btm_marker[2])
-
-      #determine range of y
-      top_y = max(top_marker[1], top_marker[3])
-      btm_y = min(btm_marker[1], btm_marker[3])
-
-      #the plus/minus 5 can be removed, but canny is
-      #picking up the edges of the markers
-      img_points.append((left_x, top_y+5, right_x, btm_y-5))
-      #img_points.append((left_x, top_y, right_x, btm_y))
-      #cv2.rectangle(img, (left_x, top_y), (right_x, btm_y), (0, 0, 255), 4)
-
-  #same concept for the structure as bounding boxes
-  img_points = np.array(img_points, dtype=dtype)
-  #tubes = [len(img_points)]
-  print('\nNumber of tubes: ' + str(len(img_points)))
-  for points in img_points:
-      #points represents area of interest
-      #i.e. under top marker and above bottom marker
-      pts_one = np.float32([[points[0], points[1]],  # leftX  topY
-                            [points[2], points[1]],  # rightX topY
-                            [points[0], points[3]],  # leftX  btmY
-                            [points[2], points[3]]]) # rightX btmY
-
-      x_diff = points[2] - points[0]
-      y_diff = points[3] - points[1]
-
-      pts_two = np.float32([[0, 0],
-                            [x_diff, 0],
-                            [0, y_diff],
-                            [x_diff, y_diff]])
-
-      matrix = cv2.getPerspectiveTransform(pts_one, pts_two)
-      tube = cv2.warpPerspective(np.copy(img), matrix, (x_diff, y_diff))
-      #tubes.append(tube)
-
-      #get edges on the tube
-      #might need gray scale/median filter?
-      edges = cv2.Canny(tube, 50, 150)
-
-      #find a horizontal line
-      #minLength is 10% of width of tube
-      lines = cv2.HoughLinesP(edges, 1, np.pi/180, 15, minLineLength=(0.1*x_diff))
-      for line in lines:
-        x1, y1, x2, y2 = line[0]
-
-        #transform the points on original image
-        x1 += points[0]
-        x2 += points[0]
-        y1 += points[1]
-        y2 += points[1]
-        if 85 <= angle(line) <= 95:
-          #draw line the entire width of tube
-          cv2.line(img, (points[0], y1), (points[2], y2), (255, 0, 0), 7)
-
-  return cv2.resize(img, dim)
+    # floats up to 4 decimal places
+    return water_lvl_percents
 
 
