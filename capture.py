7dd3d96798f5ae73924e977bae726179d5406191
Tucker Bergquist <61253021+Tucker-WB@users.noreply.github.com>
2020-05-06 16:57:33 -0500

    Change hardcoded_image to False

--- capture.py
+++ capture.py
@@ -1,8 +1,8 @@
 import cv2
 from image_processing.marker import process_image
 
-def capture_and_save(img, should_return_image = False):
-  processed_image = process_image(img, hardcoded_image=True, should_return_image=True)
+def capture_and_save(img, should_return_image = True):
+  processed_image = process_image(img, hardcoded_image=False, should_return_image=True)
   cv2.imwrite("images/last.png", resized_image(processed_image, scale=0.5))
 
 def resized_image(img, scale = 1.0):
